528f45a1e7466ea6db6cb6b647b0e069b91364ea
dpwt1997 <danilapatricia18@gmail.com>
2026-06-13 18:51:57 +0100

    Update script.js
    function adicionarAoCarrinho(produto) {
        let carrinho = JSON.parse(localStorage.getItem("carrinho") || "[]");
    
        const existente = carrinho.find(item => item.id === produto.id);
    
        if (existente) {
            existente.qty += 1;
        } else {
            carrinho.push({
                id: produto.id,
                nome: produto.nome,
                preco: produto.preco,
                qty: 1
            });
        }
    
        localStorage.setItem("carrinho", JSON.stringify(carrinho));
        alert("Produto adicionado ao carrinho!");
    }
    
    
    Signed-off-by: dpwt1997 <danilapatricia18@gmail.com>

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+function adicionarAoCarrinho(produto) {
+    let carrinho = JSON.parse(localStorage.getItem("carrinho") || "[]");
 
-card.onclick = () => {
-    window.location.href = "produto.html?id=" + produto.id;
-};
+    const existente = carrinho.find(item => item.id === produto.id);
+
+    if (existente) {
+        existente.qty += 1;
+    } else {
+        carrinho.push({
+            id: produto.id,
+            nome: produto.nome,
+            preco: produto.preco,
+            qty: 1
+        });
+    }
+
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+    alert("Produto adicionado ao carrinho!");
+}
